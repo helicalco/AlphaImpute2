a8b0921870d70cf9c9e379249f211fc999b2ea8a
Dan Garrick <dan@helicalco.com>
2023-03-25 10:26:27 +0000

    use requirements.txt to get dependencies since latest numba is broken

--- local_install.sh
+++ local_install.sh
@@ -10,7 +10,7 @@ echo date = \"$date\" >> src/alphaimpute2/Imputation/version.py
 
 python3 -m venv env
 source env/bin/activate
-# pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 pip3 install build
 python -m build
 pip3 install dist/*.whl
